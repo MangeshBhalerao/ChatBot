db5dee62980f222fec3f6c0da5aa28c13569c86e
MangeshBhalerao <mangeshbhalerao523@gmail.com>
2025-08-31 22:31:27 +0530

    Endpoint api created

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
     return 'Hello this is home'
+
+@app.route("/chat" , methods = ["POST"])
+def chat():
+    if request.method == "POST":
+        message = request.json.get('message')
+        return {f"You said: {message}"}
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
