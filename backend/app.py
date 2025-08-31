d07b12ded55f349d44ceb99fda149f661f65df99
MangeshBhalerao <mangeshbhalerao523@gmail.com>
2025-08-31 23:00:30 +0530

    Trying the ai response , connecting TinyLlama to our app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,28 @@
 from flask import Flask,request
+from models.ai_models import load_model
+import torch
 
 app = Flask(__name__)
+
+print("Loading AI Model...")
+tokenizer , model = load_model()
+print("AI Model Loaded Successfully")
+
+def generate_ai_response(user_message):
+    input = tokenizer.encode(user_message , return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model.generate(
+            inputs, 
+            max_length=100, 
+            num_return_sequences=1,
+            temperature=0.7
+        )
+    
+    # Decode the response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
+
 
 @app.route("/")
 def Home():
@@ -9,8 +31,11 @@ def Home():
 @app.route("/chat" , methods = ["POST"])
 def chat():
     if request.method == "POST":
-        message = request.json.get('message')
-        return {f"You said: {message}"}
+        user_message = request.json.get('message')
+    
+        ai_response = generate_ai_response(user_message)
+
+        return {"response": ai_response}
     
 
 if __name__ == "__main__":
---
+++ backend/models/ai_models.py
@@ -0,0 +1,8 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+def load_model():
+    model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    return tokenizer, model
